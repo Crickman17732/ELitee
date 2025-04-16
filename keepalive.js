4906894a9c3f1c6265e7fe21ef44d69540ac7a5c
Crickman17732 <crickman17732@gmail.com>
2025-04-16 22:33:38 +0530

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (_, res) => res.send("✅ Gitpod is alive"));
+app.get("/", (_, res) => res.send("✅ Gitpod alive"));
 
 app.listen(PORT, () => {
   console.log(`🟢 Keepalive running on port ${PORT}`);
